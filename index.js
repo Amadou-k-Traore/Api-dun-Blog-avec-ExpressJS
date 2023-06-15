7c96a5dbe3d00a542e0316e2f13acf3fb64ef845
Amadou-k-Traore <amadoukabinetraore96@gmail.com>
2023-06-15 20:24:42 +0000

    ExpressJS

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/api/article', (request, response) => {
         
         const sql = 'INSERT INTO article (Titre, Résumé, Contenu, Auteur, Date_de_création, Date_de_mis_à_jour) VALUES (?, ?, ?, ?, ?, ?)'
 
-        const params = [Titre.article, Résumé.article, Contenu.article, Date_de_création.article, Date_de_mis_à_jour.article]
+        const params = [article.Titre, article.Résumé, article.Contenu, article.Auteur, article.Date_de_création, article.Date_de_mis_à_jour]
 
         Db.run(sql, params, (error, rows) => {
 
@@ -59,7 +59,7 @@ app.get('/api/article', (request, response) => {
 })
 
 
-//Afficher une article 
+//Afficher une seule article 
 
 app.get('/api/article/:id', (request, response) => {
 
@@ -79,7 +79,7 @@ app.get('/api/article/:id', (request, response) => {
         } else {
             response
                 .status(200)
-                .json({ message: "Votre article est: ", data: result })
+                .json({ message: "l'article numero  "  + IDarticle +  " est", data: result })
         }
     })
 })
@@ -89,29 +89,49 @@ app.get('/api/article/:id', (request, response) => {
 app.put('/api/article/:id', (request, response) => {
     const IDarticle = request.params.id
 
-    const article = { Titre, Résumé, Contenu, Auteur, Date_de_création, Date_de_mis_à_jour } = request.body
+    const article = {Titre, Résumé, Contenu, Auteur, Date_de_création, Date_de_mis_à_jour} = request.body
 
-    if (!Titre || !Résumé || !Contenu || !Date_de_création || !Date_de_mis_à_jour) {
-        response.json({ message: "Veillez remplir tous les champs" })
+    if (!Titre || !Résumé || !Contenu || !Auteur || !Date_de_création || !Date_de_mis_à_jour) {
+        response
+            .json({ message: "Veillez remplir tous les champs" })
         return
 
     } else {
 
-        const sql = 'UPDATE article SET Titre = ?, Résume = ?, Contenu = ?, Auteur = ?, Date_de_création = ?, Date_de_mis_à_jour = ?'
+        const sql = 'UPDATE article SET Titre = ?, Résumé = ?, Contenu = ?, Auteur = ?, Date_de_création = ?, Date_de_mis_à_jour = ?'
 
-        const params = [Titre.article, Résumé.article, Contenu.Auteur, Date_de_création.article, Date_de_mis_à_jour.article]
+        const params = [article.Titre, article.Résumé, article.Contenu, article.Auteur, article.Date_de_création, article.Date_de_mis_à_jour]
 
-        Db.run(sql, params, (error, resultat) => {
+        Db.run(sql, params, (error, result) => {
 
             if (error) {
                 response.status(400)
                 console.error(error.message)
             } else {
-                response.status(200).json({message:"Mis à jour réussit"})
+                response.status(200).json({message:"Mis à jour de l'article numero "  + IDarticle  +  " réussit"})
             }
         })
-    }
+    }  
 } )
 
+//Supression d'une article
+
+app.delete('/api/article/:id', (request, response) => {
+    const IDarticle = request.params.id
+    const sql = 'DELETE FROM article WHERE id = ?'
+
+    Db.run(sql, IDarticle, (error, result) => {
+        if (error) {
+            console.error(error.message)
+            response
+                .json({ error: "Veillez entrer un identifiant existant" })
+            
+        } else {
+            response
+                .status(200)
+            .json({message:"article numéro " + IDarticle + " supprimer", data: this.changes})
+        }
+    })
+})
 
 app.listen(PORT, console.log("connexion sur le Port " + PORT))
